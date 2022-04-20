86660b8b5827779abe463433da290241e2f38560
ssoyeong <jsy171771@gmail.com>
2022-04-20 19:36:34 +0900

    Update BJ12865.java -  평범한 배낭 (Dynamic Programming)

--- BOJ/BJ12865.java
+++ BOJ/BJ12865.java
@@ -7,44 +7,36 @@ import java.util.StringTokenizer;
 // 평범한 배낭
 
 public class BJ12865 {
-
+	
 	static int n;
-	static int max;
+	static int k;
 	static int[] weight;
 	static int[] value;
-	static int[][] dp;
+	static int[] dp;
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
-		max = Integer.parseInt(st.nextToken());
-		weight = new int[n+1];
-		value = new int[n+1];
-		dp = new int[n+1][max+1];
+		k = Integer.parseInt(st.nextToken());
+		weight = new int[k + 1];
+		value = new int[n];
+		dp = new int[k + 1];
 		
-		for(int i = 1; i <= n; i++) {
+		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			weight[i] = Integer.parseInt(st.nextToken());
-			value[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i = 1; i <= n; i++) {
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			weight[i] = w;
+			value[i] = v;
 			
-			int w = weight[i];
-			int v = value[i];
-			
-			for(int j = 1; j <= max; j++) {
-				
-				dp[i][j] = dp[i-1][j];
-				if(j - w >= 0) dp[i][j] = Math.max(dp[i-1][j], v + dp[i-1][j-w]);
+			for(int j = k; j >= w; j--) {
+				dp[j] = Math.max(v + dp[j - w], dp[j]);
 			}
-			
 		}
 		
-
-		System.out.println(dp[n][max]);
+		System.out.println(dp[k]);
 	}
-	
+
 }
