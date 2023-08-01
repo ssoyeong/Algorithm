b80189ecc227eec806c99f9d58db017166345140
Soyeong Jeon <62373980+ssoyeong@users.noreply.github.com>
2023-08-01 15:12:35 +0900

    Update BJ2615.java

--- BOJ/BJ2615.java
+++ BOJ/BJ2615.java
@@ -1,15 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-// 오목
-
-public class BJ2615 {
+public class Main {
 	
 	static final int SIZE = 19;
 	static int[][] board = new int[20][20];
 	static boolean[][] visited = new boolean[20][20];
-	static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+	static int[] dx = {-1, 0, 1, 1};
+	static int[] dy = {1, 1, 1, 0};
 
 	public static void main(String[] args) throws Exception {
 		
@@ -34,7 +32,7 @@ public class BJ2615 {
 				
 				if(!visited[i][j] && (board[i][j] == 1 || board[i][j] == 2)) {		// (i, j)칸에서 탐색을 시작할 수 있다면
 					
-					for(int d = 0; d < 8; d++) {
+					for(int d = 0; d < 4; d++) {
 						int xx = i + dx[d];
 						int yy = j + dy[d];
 						if(xx < 1 || xx > SIZE || yy < 1 || yy > SIZE) continue;
