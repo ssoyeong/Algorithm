8424e768c0b277380de9e20f07de6ebefe8df93f
Soyeong Jeon <62373980+ssoyeong@users.noreply.github.com>
2023-08-03 15:32:50 +0900

    SWEA2001.java - 파리 퇴치 (Dynamic Programming)

--- SWEA/SWEA2001.java
+++ SWEA/SWEA2001.java
@@ -1,66 +1,73 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 // 파리 퇴치
 
-class SWEA2001 {
-    
-    static int t;
-    static int n, m;
-    static int[][] arr;
-    static int max;
+public class SWEA2001 {
 	
-    public static void main(String[] args) throws Exception {
-    	
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        
-        t = Integer.parseInt(br.readLine());
-        for(int tc = 0; tc < t; tc++) {
-        	
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            arr = new int[n][n];
-            max = 0;
-            
-            for(int i = 0; i < n; i++) {
-            	st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < n; j++) {
-                	arr[i][j] = Integer.parseInt(st.nextToken());
+	static int tc;
+	static int n, m;
+	static int[][] board;
+	static int[][] matrix;
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		tc = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		for(int t = 1; t <= tc; t++) {
+
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			board = new int[n + 1][n + 1];
+			matrix = new int[n + 1][n + 1];	
+
+			for(int i = 1; i <= n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 1; j <= n; j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			int ans = solution();
+			sb.append("#" + t + " " + ans + "\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static int solution() {
+
+		makeMatrix();
+
+		// m * m 크기의 최댓값 구하기
+		int max = 0;
+		for(int i = m; i <= n; i++) {
+			for(int j = m; j <= n; j++) {
+				// 왼쪽 하단을 (i, j)를 기준으로 해서 m 크기의 파리채
+				max = Integer.max(max, matrix[i][j] - matrix[i - m][j] - matrix[i][j - m] + matrix[i - m][j - m]);
+			}
+		}
+
+		return max;
+	}
+
+	private static void makeMatrix() {
+
+		for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == 1) {
+                    matrix[i][j] = matrix[i][j-1] + board[i][j];
+                }
+                else if(j == 1) {
+                    matrix[i][j] = matrix[i-1][j] + board[i][j];
+                }
+                else {
+                    matrix[i][j] = matrix[i-1][j] + matrix[i][j-1] + board[i][j] - matrix[i-1][j-1];
                 }
             }
-            
-            solution();
-           
-            sb.append(String.format("#%d %d\n", (tc+1), max));
         }
-        
-        System.out.println(sb.toString()); 
-    }
-    
-    private static void solution() {
-    	
- 
-        for(int i = 0; i < n - m + 1; i++) {
-        	for(int j = 0; j < n - m + 1; j++) {
-            	int result = calculate(i, j);
-                if(max < result) max = result;
-            }
-        }
-    }
-    
-    private static int calculate(int x, int y) {
-    	
-        int total = 0;
-        for(int i = x; i < x + m; i++) {
-            for(int j = y; j < y + m; j++) {
-            	total += arr[i][j];
-            }
-        }
-        
-        return total;
-    }
+	}
 }
